5be260cf360fbb52f8d03b8e391ce4e7a40fce2b
Taha-the-coder <78846529+Taha-the-coder@users.noreply.github.com>
2024-09-12 16:56:35 +0600

    Update transmiter_code.ino

--- other/transmiter_code.ino
+++ other/transmiter_code.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include <MPU6050.h>
 
-char yawString[10];  // Buffer to hold the formatted yaw value
 
 MPU6050 mpu;
 
@@ -10,12 +9,11 @@ unsigned long timer = 0;
 float timeStep = 0.01;
 
 float yaw = 0;
-unsigned long lastTransmitTime = 0;  // Time of the last transmission
-unsigned long transmitInterval = 100;  // Interval in milliseconds between transmissions
+
 
 void setup()
 {
-  Serial.begin(9600);  // Initialize serial communication
+  Serial.begin(115200);  // Initialize serial communication
   mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G);
   mpu.calibrateGyro();
   mpu.setThreshold(3);
@@ -26,23 +24,12 @@ void loop()
   timer = millis();
   Vector norm = mpu.readNormalizeGyro();
   yaw = yaw + norm.ZAxis * timeStep;
-
-  // Check if it's time to transmit data
-  if (millis() - lastTransmitTime >= transmitInterval) {
-    // Convert yaw to integer
-    int yawInt = (int)yaw;
-
-    // Convert the integer yaw to string
-    sprintf(yawString, "%d", yawInt);
-
-    // Send the yaw value as a string over serial
-    Serial.write(yawString, strlen(yawString));  // Send the string
-    Serial.write('\n');  // Send a newline character for line separation (optional)
-
-    // Update the time of the last transmission
-    lastTransmitTime = millis();
+  int yawInt = abs((int)yaw);
+  if (yawInt % 90 > 87 || yawInt % 90 < 3) {
+    Serial.write('1'); // Print 0 if the angle is a multiple of 90
+  } else {
+    Serial.write('0'); // Print 1 if the angle is not a multiple of 90
   }
-
-  // Ensure consistent timing for the sensor reading
+  Serial.write('\n');
   delay((timeStep * 1000) - (millis() - timer));
 }
