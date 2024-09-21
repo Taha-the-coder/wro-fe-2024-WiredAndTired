7685f1a02984a5f876d103e2c9e4e3ef042be20e
Taha-the-coder <78846529+Taha-the-coder@users.noreply.github.com>
2024-09-21 18:46:50 +0600

    Update WROCRACK.ino

--- src/WROCRACK.ino
+++ src/WROCRACK.ino
@@ -28,10 +28,12 @@ Adafruit_SSD1306 display(OLED_RESET);
 //husky
 HUSKYLENS huskylens;
 void printResult(HUSKYLENSResult result);
-int id , color;
+int id , husky;
 int red = 1;
 int green = 2;
 int noblocks = 0;
+int color = 2;
+int xpos = 1;
 
 // servo
 Servo myservo;
@@ -54,13 +56,13 @@ char ang;
 void setup() {
   //attach components
   Serial.begin(115200);
- 
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   Wire.begin();
   huskylens.begin(Wire);
   myservo.attach(5);
-  
+
   //pinmode
   for (int i = 0; i < numsonar + 1; i++) {
     pinMode(trigPins[i], OUTPUT);
@@ -70,36 +72,50 @@ void setup() {
   pinMode(8, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(A3, INPUT);
   mySerial.begin(115200);
 
 
   //menu();
-  turn(0);    
-  motor(100);
+  turn(0);
 
 
-  
+
+
 }
 
 //mainloop
 void loop() {
-//motortest();
-//Serial.println(sonar(2));
-//firstround();
-eye = analogRead(A3);
-if(eye > 30){
-turn(0);
-ang = getangle();
-}
-if(eye < 30){
-  turn(-25);
-  while(ang != '0'){
-    ang = getangle();
+  motor(0);
+  husky = huskyread(color);
+  Serial.println(husky);
+
+  while (husky != -1) {
+    husky = huskyread(color);  // Update husky
+    Serial.println(husky);
+
+    // Inner loop for husky == 1 case
+    while (husky == 1) {
+      motor(0);
+      delay(1000);
+      turn(25);
+      delay(500);
+      motor(120);
+      delay(1000);
+      motor(0);
+      delay(1000);
+      turn(-25);
+      delay(500);
+      motor(120);
+      delay(1000);
+
+      husky = huskyread(color);  // Update husky after each set of commands
+      Serial.println(husky);
+
+      // If husky changes, break out of the inner loop
+      if (husky != 1) {
+        break;
+      }
+    }
   }
-  ang = getangle();
-  while(ang != '1'){
-    ang = getangle();
-    turn(-25);
-  }
-}
 }
