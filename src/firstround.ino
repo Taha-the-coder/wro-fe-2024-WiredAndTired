831c30d4f7579fee175bd28763df953ff7dbb989
Taha-the-coder <78846529+Taha-the-coder@users.noreply.github.com>
2024-09-21 18:47:08 +0600

    Update firstround.ino

--- src/firstround.ino
+++ src/firstround.ino
@@ -1,32 +1,24 @@
-void firstround() {
-  while (clockwise) {
-    motor(120);
-    while (sonar(1) > 70) {
-      while (getangle() != 0) {
-        turn(25);
-      }
-    }
-    while (sonar(1) < 70) {
-      turn(0);
-      while(counter == 12){
-        while(analogRead(A6) > startingpos){}
-        motor(0);
-      }
-    }
+void firstround(){
+  eye = sonar(2);
+if(eye < 60){
+  motor(120);
+turn(0);
+ang = getangle();
+}
+if(eye > 60){
+  delay(20);
+  eye = sonar(2);
+  if(eye > 60){
+    motor(150);
+    turn(-25);
+  while(ang != '0'){
+    ang = getangle();
   }
-  while (counterclockwise) {
-    motor(120);
-    while (sonar(2) > 70) {
-      while (getangle() != 0) {
-        turn(-25);
-      }
-    }
-    while (sonar(2) < 70) {
-      turn(0);
-      while(counter == 12){
-        while(analogRead(A6) > startingpos){}
-        motor(0);
-      }
-    }
+  ang = getangle();
+  while(ang != '1'){
+    ang = getangle();
+    turn(-25);
   }
+  }
+}
 }
